1b4a875a9a8d73707b2e9bf1c48bc7fb1671d0f9
vbrovak <v.brovak@sigma-it.ru>
2023-06-12 17:43:17 +0300

    hw-05

---
+++ hw05-gradle/src/main/java/ru/otus/Main.java
@@ -0,0 +1,11 @@
+package ru.otus;
+
+public class Main {
+    public static void main(String[] args) {
+        TestLoggingInterface testLoggingInterface = (TestLoggingInterface) Ioc.createTestLoggingClass();
+        testLoggingInterface.calculation(6);
+        testLoggingInterface.calculation(6, 5);
+        testLoggingInterface.calculation(6, 5, 4);
+    }
+
+}
